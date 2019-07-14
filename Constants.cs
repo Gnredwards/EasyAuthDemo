12419766652eb4afc067bdf571c5b8d7f4504a01
nick.edwards <lmenredwards@yahoo.com.au>
2019-07-14 16:30:51 +1000

    Update constants

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,7 @@ namespace EasyAuthDemo
 {
     public static class Constants
     {
-        public static string BlazorWebsiteURL => "https://<client side blazor site url>/";       
+        public static string BlazorWebsiteURL => "https://<client side blazor site url>";       
         public static string AzureFunctionAuthURL => "https://<your Azure function name>.azurewebsites.net";
         public static string AuthMeEndpoint => "/.auth/me";
         public static string LogOutEndpoint => "/.auth/logout";
